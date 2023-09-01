b00926637202f6c523a1d2950e8d170cfa5fdfa3
GeniaPiano <genia.piano@hotmail.com>
2023-09-02 00:17:48 +0200

    Added rate limiter

--- index.ts
+++ index.ts
@@ -1,7 +1,8 @@
 import express, {json} from 'express';
 import cors from 'cors';
 import 'express-async-errors';
-import {handleError, ValidationError} from "./utils/error";
+import {handleError} from "./utils/error";
+import rateLimit from 'express-rate-limit';
 
 
 const app = express();
@@ -10,11 +11,11 @@ app.use(cors({
     origin: 'http://localhost:3000'
 }))
 app.use(json());
+app.use(rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+}))
 
-//Routers...
-// app.get('/', async (req, res)=> {
-//     throw new ValidationError('Daaaaaamn!')
-// })
 
 app.use(handleError);
 app.listen(3001, '0.0.0.0', () => {
